f95345dc5b1341b5ba1ac3ead804b1f8e7135840
Administrator <Administrator@PC-201411272220>
2017-11-10 23:19:12 +0800

    修改返回结果常量类

--- src/gitdemo/GitDemo.java
+++ src/gitdemo/GitDemo.java
@@ -4,7 +4,7 @@ public class GitDemo {
 
 	public static void main(String[] args) {
 		//test....................
-		//do.........
+		//do.........少时诵诗书所所所所所所所所所所所
 		//ddddddddddddddddddd
 		//111111111111111111111
 		System.out.println("###################################");
